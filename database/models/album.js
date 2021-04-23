7e9cf7e19e4afc0e381bc13badbf4c1abb1c1024
Tatyana Fomina <tfomina@logrocon.com>
2021-04-23 17:09:41 +0300

    Добавила модели Artist и Album

--- database/models/album.js
+++ database/models/album.js
@@ -1,1 +1,16 @@
-// Todo: create Album Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const AlbumSchema = new Schema({
+  title: {
+    type: String,
+    required: [true, "Title is required"],
+  },
+  date: Date,
+  copiesSold: Number,
+  numberTracks: Number,
+  image: String,
+  revenue: Number,
+});
+
+module.exports = AlbumSchema;
--- database/models/artist.js
+++ database/models/artist.js
@@ -1,1 +1,26 @@
-// Todo: Create Artist Model
+const mongoose = require("mongoose");
+const AlbumSchema = require("./album");
+const Schema = mongoose.Schema;
+
+const ArtistSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+  },
+  age: {
+    type: Number,
+    validator: (age) => age > 0,
+  },
+  yearsActive: Number,
+  image: String,
+  genre: String,
+  website: String,
+  netWorth: Number,
+  labelName: String,
+  retired: Boolean,
+  albums: [AlbumSchema],
+});
+
+const Artist = mongoose.model("artist", ArtistSchema);
+
+module.exports = Artist;
