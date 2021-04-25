aa50d461d8ae4fefa1415a7f5fd1528c5682b059
Tatyana Fomina <tatyana.fomina.1986@gmail.com>
2021-04-25 16:56:48 +0300

    Добавила GetYearsActiveRange query

--- database/queries/GetYearsActiveRange.js
+++ database/queries/GetYearsActiveRange.js
@@ -1,4 +1,4 @@
-const Artist = require('../models/artist');
+const Artist = require("../models/artist");
 
 /**
  * Finds the lowest and highest yearsActive of artists in the Artist collection
@@ -6,4 +6,16 @@ const Artist = require('../models/artist');
  * containing the min and max yearsActive, like { min: 0, max: 14 }.
  */
 module.exports = () => {
+  return Artist.aggregate([
+    {
+      $group: {
+        _id: null,
+        min: { $min: "$yearsActive" },
+        max: { $max: "$yearsActive" },
+      },
+    },
+  ]).then((groups) => ({
+    min: groups[0].min,
+    max: groups[0].max,
+  }));
 };
