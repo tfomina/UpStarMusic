ac0f0916793f848d7a7cd59ec11cfbacf0ea2b93
Tatyana Fomina <tatyana.fomina.1986@gmail.com>
2021-04-25 16:56:20 +0300

    Добавила GetAgeRange query

--- database/queries/GetAgeRange.js
+++ database/queries/GetAgeRange.js
@@ -1,4 +1,4 @@
-const Artist = require('../models/artist');
+const Artist = require("../models/artist");
 
 /**
  * Finds the lowest and highest age of artists in the Artist collection
@@ -6,4 +6,34 @@ const Artist = require('../models/artist');
  * containing the min and max ages, like { min: 16, max: 45 }.
  */
 module.exports = () => {
+  return Artist.aggregate([
+    {
+      $group: {
+        _id: null,
+        min: { $min: "$age" },
+        max: { $max: "$age" },
+      },
+    },
+  ]).then((groups) => ({
+    min: groups[0].min,
+    max: groups[0].max,
+  }));
+
+  // Решение из лекции
+  // const minQuery = Artist.find({})
+  //   .select("age")
+  //   .sort({ age: 1 })
+  //   .limit(1)
+  //   .then((artist) => artist[0].age);
+
+  // const maxQuery = Artist.find({})
+  //   .select("age")
+  //   .sort({ age: -1 })
+  //   .limit(1)
+  //   .then((artist) => artist[0].age);
+
+  // return Promise.all([minQuery, maxQuery]).then((result) => ({
+  //   min: result[0],
+  //   max: result[1],
+  // }));
 };
